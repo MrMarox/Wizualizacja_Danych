586d6bbd52f9866bb8853e074b039a671bfd2ac5
Marceli Sobiecki <64848895+MrMarox@users.noreply.github.com>
2022-03-16 14:54:16 +0100

    Update lekcja 3.py

--- lekcja 3.py
+++ lekcja 3.py
@@ -84,12 +84,61 @@ import math
 # print(row_kwadratowe(1, -2, 1))
 # print(row_kwadratowe(1, -4, 1))
 
-def czy_parzysta(a):
-    if a % 2 == 0:
-       return 'a jest parzyste'
+# def czy_parzysta(a):
+#     if a % 2 == 0:
+#        return 'a jest parzyste'
 
-    else:
-        return 'a jest nie parzyste'
+#     else:
+#         return 'a jest nie parzyste'
 
 
-print(czy_parzysta(2))
+# print(czy_parzysta(2))
+
+#
+# def dlugosc_odcinka(x1=1, y1=2, x2=3, y2=4):
+#     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+
+# #argumenty domyślne
+# print(dlugosc_odcinka())
+#
+# #agumenty pozycyjne
+# print(dlugosc_odcinka(4,5,6,9))
+#
+# #dwa pierwsze argumenty pozycyjne, kolejne dwa wpisane wartości
+# print(dlugosc_odcinka(1,1,y2=8, x2=7))
+#
+# #wartość przypisana do danego argumentu
+# print(dlugosc_odcinka(y2=3,x1=5,y1=6,x2=0))
+#
+# #dwa pierwsze domyślne, kolejne dwa nowe wartości wpisane
+# print(dlugosc_odcinka(y2=1,x2=6))
+
+
+# def ciag(* liczba):
+#     if len(liczba) == 0:
+#         return 0
+#     else:
+#         suma = 0
+#         for i in liczba:
+#             suma += i
+#         return suma
+#
+# print(ciag())
+# print(ciag(1,2,3,4,5,6,7,8))
+
+# def co_lubie(** rzeczy):
+#     for cos in rzeczy:
+#         print('lubie')
+#         print(cos)
+#         print('co lubie')
+#         print(rzeczy[cos])
+
+# co_lubie(gry=['planszowe','komputerowe'])
+
+
+
+
+
+
+
